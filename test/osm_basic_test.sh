9457b2966de38283e99f890ff518a0cb4b549909
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-12-20 10:31:07 +0100

    Add test for new items, more informative output

--- test/osm_basic_test.sh
+++ test/osm_basic_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ./??_* -maxdepth 2 -type f -name "*.osm" -exec sh -c 'echo {}; cat {} | grep "OSM link" | sort | uniq -cd;cat {} | grep "OSM link" | sort | uniq -cd | wc -l' \;
+find ./??_* -maxdepth 2 -type f -name "*.osm" -exec sh -c 'echo {}; cat {} | grep "OSM link" | sort | uniq -cd;echo -n "Duplicated elements: "; cat {} | grep "OSM link" | sort | uniq -cd | wc -l;echo -n "New elements: "; grep "id=\"-" {} | wc -l; echo ""' \;

8f10167c51909f4950947ada70973909040c0762
Kálmán Szalai - KAMI <kami911@gmail.com>
2018-12-23 03:22:41 +0100

    Add all elements start and formatted output

--- test/osm_basic_test.sh
+++ test/osm_basic_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find ./??_* -maxdepth 2 -type f -name "*.osm" -exec sh -c 'echo {}; cat {} | grep "OSM link" | sort | uniq -cd;echo -n "Duplicated elements: "; cat {} | grep "OSM link" | sort | uniq -cd | wc -l;echo -n "New elements: "; grep "id=\"-" {} | wc -l; echo ""' \;
+find ./??_* -maxdepth 2 -type f -name "*.osm" -exec sh -c 'echo {}; echo -n "All elements        : "; cat {} | grep "Original coordinates" | wc -l; cat {} | grep "OSM link" | sort | uniq -cd;echo -n "Duplicated elements : "; cat {} | grep "OSM link" | sort | uniq -cd | wc -l;echo -n "New elements        : "; grep "id=\"-" {} | wc -l; echo ""' \;

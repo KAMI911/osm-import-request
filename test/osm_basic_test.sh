1cbf0fdda50726c3af05d00256fbecce01017404
Kálmán Szalai - KAMI <kami911@gmail.com>
2019-01-11 12:03:34 +0100

    Update starts with summary

--- test/osm_basic_test.sh
+++ test/osm_basic_test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
 find ./??_* -maxdepth 2 -type f -name "*.osm" -exec sh -c 'echo {}; echo -n "All elements        : "; cat {} | grep "Original coordinates" | wc -l; cat {} | grep "OSM link" | sort | uniq -cd;echo -n "Duplicated elements : "; cat {} | grep "OSM link" | sort | uniq -cd | wc -l;echo -n "New elements        : "; grep "id=\"-" {} | wc -l; echo ""' \;
+
 echo "Name changes:"
 find ./??_* -maxdepth 2 -type f -name "*.osm" -exec sh -c 'grep "name                             M" {}' \;
+
+echo "SUMMARY"
+echo -n "All elements        : "
+grep  "Original coordinates" hu_*/*.osm | wc -l
+echo -n "New elements        : "
+grep "id=\"-" hu_*/*.osm | wc -l
